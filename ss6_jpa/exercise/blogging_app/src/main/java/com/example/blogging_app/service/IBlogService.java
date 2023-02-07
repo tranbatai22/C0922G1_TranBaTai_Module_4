ef8fc5240f65f59ce318c8a7636629d4d0ea5ace
taiem22 <tranbatai2202@gmail.com>
2023-02-07 20:47:49 +0700

    bt

---
+++ ss6_jpa/exercise/blogging_app/src/main/java/com/example/blogging_app/service/IBlogService.java
@@ -0,0 +1,17 @@
+package com.example.blogging_app.service;
+
+import com.example.blogging_app.model.Blog;
+
+import java.util.List;
+
+public interface IBlogService {
+    List<Blog> findAll();
+
+    Blog findById(int id);
+
+    void save(Blog blog);
+
+    void delete(int id);
+
+    void update(Blog blog);
+}
