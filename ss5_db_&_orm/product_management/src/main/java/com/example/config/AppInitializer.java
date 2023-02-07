73b60075e577aa8bd00cc3b64c85bcfc09626822
taiem22 <tranbatai2202@gmail.com>
2023-02-07 10:59:36 +0700

    bt

--- ss5_db_&_orm/product_management/src/main/java/com/example/config/AppInitializer.java
+++ ss5_db_&_orm/product_management/src/main/java/com/example/config/AppInitializer.java
@@ -1,2 +1,20 @@
-package com.example.config;public class AppInitializer {
+package com.example.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
