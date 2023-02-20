004e0a286d453605c3daa1f48b47834ab4f20be9
taiem22 <tranbatai2202@gmail.com>
2023-02-20 23:57:07 +0700

    bt

--- case_study/furama_resort/src/main/java/com/example/furama_resort/controller/customer/CustomerController.java
+++ case_study/furama_resort/src/main/java/com/example/furama_resort/controller/customer/CustomerController.java
@@ -1,8 +1,10 @@
 package com.example.furama_resort.controller.customer;
 
+import com.example.furama_resort.dto.CustomerDto;
 import com.example.furama_resort.model.customer.Customer;
 import com.example.furama_resort.service.customer.ICustomerService;
 import com.example.furama_resort.service.customer.ICustomerTypeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,9 +12,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/customer")
@@ -32,4 +37,37 @@ public class CustomerController {
         model.addAttribute("searchEmail", searchEmail);
         return "/customer/list";
     }
+
+    @GetMapping("/create")
+    public String showFormCreate(Model model){
+        model.addAttribute("customerDto", new CustomerDto());
+        model.addAttribute("customerTypeList", customerTypeService.findAll());
+        return "/customer/create";
+    }
+
+//    @PostMapping("/create")
+//    public String saveCus(@ModelAttribute @Validated CustomerDto customerDto, BindingResult bindingResult,
+//                          RedirectAttributes redirectAttributes, Model model) {
+//
+//        Map<String, String> messError = customerService.messError(customerDto);
+//        new CustomerDto().validate(customerDto, bindingResult);
+//        model.addAttribute("customerDtoc",customerDto);
+//        if (bindingResult.hasErrors()) {
+//            model.addAttribute("customerTypeList", customerTypeService.findAll());
+//            return "customer/create";
+//        }
+//
+//        if (messError.isEmpty()) {
+//            Customer customer = new Customer();
+//            BeanUtils.copyProperties(customerDto, customer);
+//            customerService.save(customer);
+//            redirectAttributes.addFlashAttribute("message", "Thêm mới khách hàng thành công!");
+//        } else {
+//            for (Map.Entry<String, String> error : messError.entrySet()) {
+//                bindingResult.rejectValue(error.getKey(), error.getValue());
+//            }
+//        }
+//
+//        return "redirect:/customer";
+//    }
 }
---
+++ case_study/furama_resort/src/main/java/com/example/furama_resort/dto/CustomerDto.java
@@ -0,0 +1,102 @@
+package com.example.furama_resort.dto;
+
+import com.example.furama_resort.model.customer.CustomerType;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class CustomerDto implements Validator {
+    private int id;
+    private String name;
+    private String dateOfBirth;
+    private int gender;
+    private String idCard;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private CustomerType customerType;
+
+    public CustomerDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        CustomerDto customerDto = (CustomerDto) target;
+    }
+}
